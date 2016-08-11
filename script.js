7684cd1086ab4b2b556630ae0ddea1d3cb074045
Samantha Musselman <samantha.musselman212@gmail.com>
2016-08-10 20:34:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by samanthamusselman on 8/10/16.
+ */
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.980, lng: 109.743},
+        zoom: 8
+    });
+}
