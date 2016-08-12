da6443ede9d44ea8741aeaab93c885d15f4b5e4a
Samantha Musselman <samantha.musselman212@gmail.com>
2016-08-12 16:36:56 -0500

    Map styling

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 35.980, lng: 109.743},
-        zoom: 8
+        zoom: 8,
+        styles: [
+            {
+                featureType: 'all',
+                stylers: [
+                    {saturation: -45}
+                ]
+            }
+        ]
     });
 }
